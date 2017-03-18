4ebe64e58decc2c526bbff6848b191fcd71526ed
jitenderpadda <jitenderpadda@gmail.com>
2017-03-18 12:28:20 +1100

    Implemented compete()
    Implemented compete() to return random time between the MIN_TIME and MAX_TIME

--- assignment1/src/assignment1/Swimmer.java
+++ assignment1/src/assignment1/Swimmer.java
@@ -1,10 +1,19 @@
 package assignment1;
 
+import java.util.Random;
+
 public class Swimmer extends Athlete {
 
-	@Override
-	public void compete() {
-		
+	private final static int MAX_TIME =200;
+	private final static int MIN_TIME =100;
+	
+	public Swimmer(String name, int age, String state, int points) {
+		super(name, age, state, points);
 	}
 
+	@Override
+	public int compete() {
+		// Looked up from - http://stackoverflow.com/questions/20389890/generating-a-random-number-between-1-and-10-java
+		return new Random().nextInt(MAX_TIME-MIN_TIME+1)+MIN_TIME;
+	}
 }
