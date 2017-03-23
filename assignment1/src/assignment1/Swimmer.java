7d55e5aa7c80dc475ba4296d008486d4646a6863
jitenderpadda <jitenderpadda@gmail.com>
2017-03-23 14:33:20 +1100

    Update Swimmer.java

--- assignment1/src/assignment1/Swimmer.java
+++ assignment1/src/assignment1/Swimmer.java
@@ -7,12 +7,12 @@ public class Swimmer extends Athlete {
 	private final static int MAX_TIME =200;
 	private final static int MIN_TIME =100;
 	
-	public Swimmer(String name, int age, String state, int points) {
-		super(name, age, state, points);
+	public Swimmer(String id,String name,int age,String state,int points){
+		super(id,name,age,state,points);
 	}
 
 	@Override
-	public int compete() {
+	public int compete(){
 		// Looked up from - http://stackoverflow.com/questions/20389890/generating-a-random-number-between-1-and-10-java
 		return new Random().nextInt(MAX_TIME-MIN_TIME+1)+MIN_TIME;
 	}
