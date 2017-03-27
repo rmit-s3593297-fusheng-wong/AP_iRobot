f4da10a16e7463c8568aea256a6eb6b5094d20bd
jitenderpadda <jitenderpadda@gmail.com>
2017-03-28 09:25:55 +1100

    Update SuperAthlete.java

--- assignment1/src/assignment1/SuperAthlete.java
+++ assignment1/src/assignment1/SuperAthlete.java
@@ -1,10 +1,28 @@
+/*****Author - Jitender Singh Padda***********
+ *****Student Id - 3628144************************/
 package assignment1;
 
 public class SuperAthlete extends Athlete {
 	
-	@Override
-	public void compete() {
-		
-	}
+	private String gameType;
 	
+	public SuperAthlete(String id, String name, int age, String state, int points,String gameType) {
+		super(id, name, age, state, points);
+		this.gameType=gameType;
+	}
+
+	@Override
+	public int compete() {
+		Athlete superAthlete = null;
+		if(gameType=="Cycling"){
+			superAthlete=new Cyclist("id","name", 28, "state", 0);
+		}
+		else if(gameType=="Swimming"){
+			superAthlete=new Cyclist("id","name", 28, "state", 0);
+		}
+		else if(gameType=="Sprinting"){
+			superAthlete=new Cyclist("id","name", 28, "state", 0);	
+		}
+		return superAthlete.compete();
+	}
 }
