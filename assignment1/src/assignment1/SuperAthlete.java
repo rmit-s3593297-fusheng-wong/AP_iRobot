eba924c71c797336836e3177ad3323989b393d8c
jitenderpadda <jitenderpadda@gmail.com>
2017-03-29 17:40:33 +1100

    Changed Constructor & added setter for gameType

--- assignment1/src/assignment1/SuperAthlete.java
+++ assignment1/src/assignment1/SuperAthlete.java
@@ -6,22 +6,29 @@ public class SuperAthlete extends Athlete {
 	
 	private String gameType;
 	
-	public SuperAthlete(String id, String name, int age, String state, int points,String gameType) {
+	//constructor
+	public SuperAthlete(String id, String name, int age, String state, int points) {
 		super(id, name, age, state, points);
+	}
+	
+	//setter for gameType
+	public void setGameType(String gameType){
 		this.gameType=gameType;
 	}
-
+	
+	//Overriding compete() method from Athlete class
 	@Override
 	public int compete() {
 		Athlete superAthlete = null;
+		//SuperAthlete will create an instance of the type of Athlete according to gameType and call it's compete() method
 		if(gameType=="Cycling"){
-			superAthlete=new Cyclist("id","name", 28, "state", 0);
+			superAthlete=new Cyclist(getId(),getName(),getAge(),getState(),getPoints());
 		}
 		else if(gameType=="Swimming"){
-			superAthlete=new Cyclist("id","name", 28, "state", 0);
+			superAthlete=new Swimmer(getId(),getName(),getAge(),getState(),getPoints());
 		}
 		else if(gameType=="Sprinting"){
-			superAthlete=new Cyclist("id","name", 28, "state", 0);	
+			superAthlete=new Sprinter(getId(),getName(),getAge(),getState(),getPoints());
 		}
 		return superAthlete.compete();
 	}
