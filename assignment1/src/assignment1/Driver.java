686aab2eca45546071cd3d6de5d3b08502f51e35
rmit-s3593297-fusheng-wong <s3593297@student.rmit.edu.au>
2017-04-02 00:58:53 +1100

    Fixed bugs
    Changed the points when game is won
    Changed the predictions header from "Current" to "Total" to make it less ambiguous

--- assignment1/src/assignment1/Driver.java
+++ assignment1/src/assignment1/Driver.java
@@ -15,120 +15,158 @@ public class Driver {
 	private static final int EXIT_GAME = 6;
 	private int currentGame = 0;
 	private int predictions = 0;
-	Scanner sc = new Scanner(System.in);
 	
 	public void console(Game[] games, Athlete[] athletes){
-		boolean cont = true;
-		
-		do{
-			cont = mainMenu(games,athletes);
-		}while(cont);
-	}
-	
-	public int selectGame(Game[] games){
 		Scanner sc = new Scanner(System.in);
-		int selectedGame = 0;
-		
-		System.out.println("List of Games");
-		System.out.println("===================================");
-		for(int i=0;i<games.length;i++){
-			System.out.println(i + ". " + games[i].getGameID());
-		}
-		System.out.println("Select a Game: ");
-		
-		boolean isValid=false;
-		do{
-			if (sc.hasNextInt()) {
-				if(sc.nextInt()<games.length){
-					selectedGame = sc.nextInt();
-					isValid = true;
-				}
-			}
-		}while(!isValid);
-		
-		sc.close();
-		return selectedGame;
-	}
-	
-	public void predictWinner(Game game){
-		
-		System.out.println("List of athletes");
-		System.out.println("===================================");
-		game.getAthleteNames();
-		System.out.println("Select an Athlete: ");
-		boolean isValid=false;
-		do{
-			if (sc.hasNextInt()) {
-				if(sc.nextInt()<game.length){
-					game.setGamePrediction(sc.nextInt());
-					isValid = true;
-				}
-			}
-		}while(!isValid);
-		
-	}
-	
-	public void runGame(Game game){
-		predictions += game.runGame();
-	}
-	
-	public void displayAllGameResults(Game[] games){
-		
-	}
-	
-	public void displayAthletePoints(Athlete[] athletes){
-		
-		for(int i=0;i<athletes.length;i++){
-			System.out.print(i+1);
-			//System.out.print(": " + athletes[i].getName());
-		}
-	}
-	
-	private boolean mainMenu(Game[] games, Athlete[] athletes){
-		boolean cont = true;
 		displayMenu();
 		if (sc.hasNextInt()) {
-			int action = sc.nextInt();
-			switch(action){
+			switch(sc.nextInt()){
+			case EXIT_GAME:
+				System.out.println("Thank you for playing");
+				break;
 			case SELECT_GAME:
-				currentGame = selectGame(games);
-				mainMenu(games, athletes);
+				selectGame(games, athletes);
 				break;
 			case PREDICT_WINNER:
-				predictWinner(games[currentGame]);
-				mainMenu(games, athletes);
+				predictWinner(games, athletes);
 				break;
 			case START_GAME:
-				if(currentGame == 0){
-					System.out.println("Please choose a game first");
-				}else{
-					runGame(games[currentGame-1]);
-				}
-				mainMenu(games, athletes);
+				runGame(games, athletes);
 				break;
 			case DISPLAY_ALL_GAMES:
-				displayAllGameResults(games);
-				mainMenu(games, athletes);
+				displayAllGameResults(games, athletes);
 				break;
 			case DISPLAY_ALL_ATHLETES:
-				displayAthletePoints(athletes);
-				mainMenu(games, athletes);
-				break;
-			case EXIT_GAME:
-				cont = false;
+				displayAthletePoints(games, athletes);
 				break;
 			default:
 				System.out.println("Please choose a number between 1 and 6");
-				mainMenu(games,athletes);
+				console(games,athletes);
 			}
 		}else{
 			System.out.println("Please choose a valid input");
+			console(games,athletes);
 		}
-		return cont;
+		sc.close();
+	}
+	
+	private void selectGame(Game[] games, Athlete[] athletes){
+		
+		System.out.println("List of Games");
+		System.out.println("===================================");
+		String strOutput;
+		for(int i=0;i<games.length;i++){
+			strOutput = (i+1) + ". " + games[i].getGameID();
+			strOutput += " - " + games[i].getGameType();
+			System.out.println(strOutput);
+		}
+		System.out.println("Select a Game: ");
+		
+		Scanner sc1 = new Scanner(System.in);
+		
+		if (sc1.hasNextInt()) {
+			int input = sc1.nextInt();
+			if(input<=games.length){
+				//this is the integer the user sees, in array it needs to be -1
+				currentGame = input;
+				
+				//reset the games prediction
+				games[currentGame-1].setGamePrediction("");
+				console(games,athletes);
+			}else{
+				System.out.println("Please choose a number between 1 and " + games.length);
+				selectGame(games, athletes);
+			}
+		}else{
+			System.out.println("Please choose a number between 1 and " + games.length);
+			selectGame(games, athletes);
+		}
+		sc1.close();
+	}
+	
+	private void predictWinner(Game[] games, Athlete[] athletes){
+		//check that game is chosen
+		if(gameChosen()){
+			//list athletes
+			System.out.println("List of athletes");
+			System.out.println("===================================");
+			games[currentGame-1].getAthleteNames(games[currentGame-1]);
+			System.out.println("Select an Athlete: ");
+			
+			Scanner sc = new Scanner(System.in);
+			if (sc.hasNextInt()) {
+				int input = sc.nextInt();
+				if(input > 0 && input <= games[currentGame-1].getAthleteCount()){
+					//get the athletes ID
+					String athleteID = games[currentGame-1].getGameAthleteID(input-1);
+					games[currentGame-1].setGamePrediction(athleteID); 
+					
+					console(games,athletes);
+				}else{
+					System.out.println("Please choose a number between 1 and " + games[currentGame-1].getAthleteCount());
+					selectGame(games, athletes);
+				}
+				
+			}else{
+				System.out.println("Please choose a number between 1 and " + games[currentGame-1].getAthleteCount());
+				selectGame(games, athletes);
+			}
+			sc.close();
+			
+		}else{
+			System.out.println("Please choose a game first");
+			console(games,athletes);
+		}
+		
+		
+	}
+	
+	private void runGame(Game[] games,Athlete[] athletes){
+		//check that a game has been chosen
+		if(gameChosen()){
+			//check that a prediction has been made
+			if(games[currentGame-1].getGamePrediction() != ""){
+				predictions += games[currentGame-1].runGame();
+				
+				//reset game's prediction
+				games[currentGame-1].setGamePrediction("");
+				
+				//reset the chosen game
+				currentGame = 0;
+			}else{
+				System.out.println("Please predict a winner first");
+			}
+		}else{
+			System.out.println("Please choose a game first");
+		}
+		console(games,athletes);
+	}
+	
+	private void displayAllGameResults(Game[] games, Athlete[] athletes){
+		for(int i=0;i<games.length;i++){
+			if(games[i].hasResult()){
+				games[i].displayGameResults();
+			}else{
+				System.out.println(games[i].getGameID() + "has not yet been run");
+			}
+		}
+		console(games,athletes);
+	}
+	
+	private void displayAthletePoints(Game[] games, Athlete[] athletes){
+		for(int i=0;i<athletes.length;i++){
+			System.out.println("Name: " + athletes[i].getName());
+			System.out.println("===================================");
+			System.out.println("Age: " + athletes[i].getAge());
+			System.out.println("State: " + athletes[i].getState());
+			System.out.println("Points: " + athletes[i].getPoints());
+			System.out.println("");
+		}
+		console(games,athletes);
 	}
 	
 	private void displayMenu(){
-		System.out.println("Ozlympic Game");
+		System.out.println("Total successful predictions: " + predictions);
 		System.out.println("===================================");
 		System.out.println("1. Select a game to run");
 		System.out.println("2. Predict the winner of the game");
@@ -137,5 +175,14 @@ public class Driver {
 		System.out.println("5. Display the points of all athletes");
 		System.out.println("6. Exit");
 		System.out.println("Enter an option: ");
+		System.out.println(" ");
+	}
+	
+	private boolean gameChosen(){
+		if(currentGame > 0){
+			return true;
+		}else{
+			return false;
+		}
 	}
 }
--- assignment1/src/assignment1/Game.java
+++ assignment1/src/assignment1/Game.java
@@ -11,7 +11,8 @@ public class Game {
 	private Official gameOfficial;
 	private String gamePrediction;
 	private Athlete[] gameAthletes = new Athlete[MAXATHLETE];
-	private int[] gameAthletesTimes;
+	private int[] gameAthletesTimes = new int[MAXATHLETE];
+	private boolean hasResult = false; //this is for quick reference when displaying all games
 	
 	public Game(String gameID, String gameType, Official gameOfficial, Athlete[] gameAthletes){
 		this.gameID = gameID;
@@ -20,6 +21,10 @@ public class Game {
 		for(int i=0;i<gameAthletes.length;i++){
 			this.gameAthletes[i] = gameAthletes[i];
 		}
+	}
+	
+	public boolean hasResult(){
+		return this.hasResult;
 	}
 	
 	public String getGameID(){
@@ -31,30 +36,41 @@ public class Game {
 	}
 	
 	public int getAthleteCount(){
-		return this.gameAthletes.length;
+		//count the non null
+		int count = 0;
+		for(int i=0;i<this.gameAthletes.length;i++){
+			if(this.gameAthletes[i] != null){
+				count++;
+			}
+		}
+		return count;
 	}
 	
 	public String getGamePrediction(){
 		return this.gamePrediction;
 	}
 	
-	public String[] getAthleteNames(){
-		//this returns all the athlete names
-		String[] athleteNames = new String[this.gameAthletes.length];
-		
-		for(int i=0;i<this.gameAthletes.length;i++){
-			//athleteNames[i] = game.gameAthletes[i].getName();
-			athleteNames[i] = "AthleteName";
-		}
-		return athleteNames;
+	public void setGamePrediction(String athleteID){
+		this.gamePrediction = athleteID;
 	}
 	
-	public void setGamePrediction(String gamePrediction){
-		this.gamePrediction = gamePrediction;
+	public void getAthleteNames(Game game){
+		//this returns all the athlete names
+		for(int i=0;i<this.gameAthletes.length;i++){
+			//check if there is an athlete
+			if(game.gameAthletes[i] != null){
+				System.out.println((i+1)+": " + game.gameAthletes[i].getName());
+			}
+		}
+	}
+	
+	public String getGameAthleteID(int athleteNo){
+		return this.gameAthletes[athleteNo].getId();
 	}
 	
 	public int runGame(){
 		boolean isValid = true;
+		int prediction = 0;
 		
 		//check there are enough athletes
 		if(this.getAthleteCount()<4){
@@ -63,32 +79,69 @@ public class Game {
 		}
 		
 		//check for a prediction prediction
-		if(this.getAthleteCount()<4){
-			System.out.println("There are insufficient athletes to run this game");
+		if(this.gamePrediction == ""){
+			System.out.println("Please make a prediction before running a game");
 			isValid = false;
 		}
 		
-		//get times for each athlete
-		for (int i=0;i<this.getAthleteCount();i++){
-			gameAthletesTimes[i] = gameAthletes[i].compete();
-			//gameAthletes[i].setFinishTime(gameAthletesTimes[i]);
+		//after checks perform actual game
+		if(isValid){
+			//get times for each athlete
+			for (int i=0;i<this.getAthleteCount();i++){
+				//let the superathlete know the game type
+				if(gameAthletes[i] instanceof SuperAthlete){
+					((SuperAthlete) gameAthletes[i]).setGameType(this.gameType);
+				}
+				gameAthletes[i].setTime(gameAthletes[i].compete());
+			}
+			
+			//sort based on times
+			this.gameAthletes = gameOfficial.sumGame(this.gameAthletes);
+			
+			for (int i=0;i<this.getAthleteCount();i++){
+				//save the times to be called in the future
+				gameAthletesTimes[i] = gameAthletes[i].getTime();
+			}
+			
+			//set the winner's points
+			int athletePoints=0;
+			//get previous points
+			athletePoints = gameAthletes[0].getPoints();
+			//add new points
+			gameAthletes[0].setPoints(athletePoints + 5); //winner gets 5
+			athletePoints = gameAthletes[1].getPoints();
+			gameAthletes[1].setPoints(athletePoints + 2); //2nd gets 2
+			athletePoints = gameAthletes[2].getPoints();
+			gameAthletes[2].setPoints(athletePoints + 1); //3rd gets 1
+			
+			//check prediction is the same as the winner
+			if(this.gamePrediction == gameAthletes[0].getId()){
+				prediction = 1;
+			}
+			
+			//display the results
+			displayGameResults();
+			this.hasResult =  true;
 		}
 		
-		//get the winner
-		//Official.sumGame();
-		
-		//display the results
-		displayGameResults();
-		
-		int prediction = 0;
-		/*if(this.gamePrediction = gameAthletesTimes[0].getID()){
-			prediction = 1;
-		}*/
 		return prediction;
 	}
 	
-	private void displayGameResults(){
-		//
+	public void displayGameResults(){
+		//Display game details
+		System.out.println(this.gameID + " - " + this.gameType);
+		System.out.println("===================================");
+		System.out.println("Official: " + this.gameOfficial.getName());
+		
+		//display first 3 positions
+		String strOutput;
+		
+		for(int i = 0;i<3;i++){
+			strOutput = (i+1) + ": " + this.gameAthletes[i].getName() + " - ";
+			strOutput += this.gameAthletesTimes[i] + "s";
+			System.out.println(strOutput);
+		}
+		System.out.println("");
 	}
 	
 }
