25930898511308dae12f2f2ff2af18e1211ce139
jitenderpadda <jitenderpadda@gmail.com>
2017-04-02 09:07:08 +1000

    Final Commit

--- assignment1/src/assignment1/Sprinter.java
+++ assignment1/src/assignment1/Sprinter.java
@@ -4,20 +4,20 @@ package jitender;
 
 import java.util.Random;
 
-public class Sprinter extends Athlete {
-	
-	private final static int MAX_TIME =20;//Constants
-	private final static int MIN_TIME =10;//Constants
+public class Swimmer extends Athlete {
+
+	private final static int MAX_TIME =200;//Constants
+	private final static int MIN_TIME =100;//Constants
 	
 	//Constructor
-	public Sprinter(String id,String name,int age,String state,int points){
+	public Swimmer(String id,String name,int age,String state,int points){
 		super(id,name,age,state,points);
 	}
-	
+
 	//Overriding compete() method from Athlete class
 	@Override
 	public int compete(){
-		//using Random class to generate Random integers between the MIN_TIME and MAX_TIMEs
-		return new Random().nextInt(MAX_TIME-MIN_TIME+1)+MIN_TIME; 
+		//using Random class to generate Random integers between the MIN_TIME and MAX_TIME
+		return new Random().nextInt(MAX_TIME-MIN_TIME+1)+MIN_TIME;
 	}
 }
