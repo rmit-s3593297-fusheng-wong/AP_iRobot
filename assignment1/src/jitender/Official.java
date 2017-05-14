2690377827bc10c86d1e403e9e01d9f8ce80ab4e
jitenderpadda <jitenderpadda@gmail.com>
2017-05-14 13:43:55 +1000

    Update Official.java

--- assignment1/src/jitender/Official.java
+++ assignment1/src/jitender/Official.java
@@ -2,32 +2,18 @@
  *****Student Id - 3628144************************/
 package jitender;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Official extends Participant {
 	
 	//Constructor
 	public Official(String id,String name, int age, String state) {
 		super(id,name,age,state);
 	}
-	
-	public Athlete[] sumGame(Athlete[] athleteList){
-		//using Bubble Sort to sort athleteList in ascending order of their Time
-		for(int i=0;i<athleteList.length-1;i++){
-			Athlete temp=null;
-			for(int j=0;j<athleteList.length-i-1;j++){
-				//Exception handling for possible Null references in array
-				try{	
-					if(athleteList[j].getTime() > athleteList[j+1].getTime()){
-						temp=athleteList[j];
-						athleteList[j]=athleteList[j+1];
-						athleteList[j+1]=temp;
-					}
-				}
-				catch(Exception NullPointerException){
-					continue;
-				}
-			}
-		}
+	public ArrayList<Athlete> sumGame(ArrayList<Athlete> athleteList){
+		//Using Collections.sort to sort athleteList as Athlete implements Comparator
+		Collections.sort(athleteList);
 		return athleteList;//sorted AthleteList in ascending order of their Time		
 	}
 }
-
