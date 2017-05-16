c4eab8c16b6780273aa2246846c33dbc6375c3be
jitenderpadda <jitenderpadda@gmail.com>
2017-05-16 18:31:51 +1000

    Add files via upload

--- assignment1/src/main/Ozlympic.java
+++ assignment1/src/main/Ozlympic.java
@@ -14,18 +14,13 @@ import javax.swing.JOptionPane;
 import javax.swing.Timer;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.ProgressIndicator;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import jitender.Athlete;
 import jitender.DatabaseHelper;
 import jitender.Game;
+import jitender.GameAnimation;
 import jitender.NoRefereeException;
 import jitender.Official;
 import jitender.TooFewAthleteException; 
@@ -33,15 +28,13 @@ import jitender.TooFewAthleteException;
 public class Ozlympic extends Application {
 	private ArrayList<Athlete> athleteList;
 	private ArrayList<Official> officialList;
-	private HashMap<Athlete,ProgressBar> athleteProgBarMap;
-	private HashMap<Athlete,ProgressIndicator> athleteProgIndMap;
 	private Timer timer;
 	
 	public Ozlympic() throws ClassNotFoundException, FileNotFoundException, SQLException{
 		athleteList=new ArrayList<Athlete>();
 		officialList=new ArrayList<Official>();
-		athleteProgBarMap=new HashMap<Athlete,ProgressBar>();
-		athleteProgIndMap=new HashMap<Athlete,ProgressIndicator>();
+		new HashMap<Athlete,ProgressBar>();
+		new HashMap<Athlete,ProgressIndicator>();
 		initiateParticipants();
 		
 	}
@@ -123,44 +116,8 @@ public class Ozlympic extends Application {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		Group root = new Group();
-        Scene scene = new Scene(root, 300, 250);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Ozlympic");
-        
-        ArrayList<HBox> HBoxes= new ArrayList<HBox>();
-        HBox gameHeader=new HBox();
-        gameHeader.getChildren().add(new Label(finishedGame.getGameType()));
-        gameHeader.setAlignment(Pos.CENTER);
-        HBoxes.add(gameHeader);
-        for (Athlete athlete : finishedGame.getGameAthletes()) {
-            Label label =new Label();
-            label.setText(athlete.getName()+":");
- 
-            ProgressBar pBar =new ProgressBar();
-            pBar.setProgress(0f);
- 
-            ProgressIndicator pInd=new ProgressIndicator();
-            pInd.setProgress(0f);
-            HBox hBox=new HBox();
-            hBox.setSpacing(5);
-            hBox.setAlignment(Pos.CENTER);
-            hBox.getChildren().addAll(label,pBar,pInd);
-            HBoxes.add(hBox);
-            
-            athleteProgBarMap.put(athlete,pBar);
-            athleteProgIndMap.put(athlete,pInd);
-            
-        }
-        progress(athleteProgBarMap,athleteProgIndMap);
-        final VBox vb = new VBox();
-        vb.setSpacing(5);
-        vb.getChildren().addAll(HBoxes);
-        scene.setRoot(vb);
-        primaryStage.setMinHeight(450);
-        primaryStage.setMinWidth(300);
-        primaryStage.show();
+		GameAnimation gameAnimate=new GameAnimation(primaryStage, finishedGame);
+		gameAnimate.runAnimation();
 		
 	}
 	//Listener
